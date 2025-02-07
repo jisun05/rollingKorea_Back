07535718962f0730dfb3819986a7c190647a36a6
jisun05 <jisunnala@gmail.com>
2025-02-07 17:14:20 +0900

    Add about place service  v.0.01

--- src/main/java/history/traveler/rollingkorea/place/repository/PlaceRepository.java
+++ src/main/java/history/traveler/rollingkorea/place/repository/PlaceRepository.java
@@ -1,13 +1,10 @@
 package history.traveler.rollingkorea.place.repository;
 
-import history.traveler.rollingkorea.place.controller.response.PlaceResponse;
 import history.traveler.rollingkorea.place.domain.Place;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository; // Spring Data JPA의 JpaRepository 임포트, 이 인터페이스는 CRUD 작업을 위한 기본 메소드를 제공
-/// Spring의 @Repository 어노테이션 임포트,이 어노테이션은 해당 인터페이스가 데이터 접근 레이어임을 나타냄
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/history/traveler/rollingkorea/place/service/Implementation/PlaceServiceImpl.java
+++ src/main/java/history/traveler/rollingkorea/place/service/Implementation/PlaceServiceImpl.java
@@ -4,17 +4,23 @@ package history.traveler.rollingkorea.place.service.Implementation;
 import history.traveler.rollingkorea.place.controller.request.PlaceCreateRequest;
 import history.traveler.rollingkorea.place.controller.response.PlaceResponse;
 import history.traveler.rollingkorea.place.domain.Place;
+import history.traveler.rollingkorea.place.repository.PlaceRepository;
 import history.traveler.rollingkorea.place.service.PlaceService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.awt.print.Pageable;
 import java.io.IOException;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PlaceServiceImpl implements PlaceService {
+
+    private final PlaceRepository placeRepository;
+
+
     @Override
     public void placeCreate(PlaceCreateRequest placeCreateRequest, List<MultipartFile> imagePath) throws IOException {
 
@@ -27,6 +33,10 @@ public class PlaceServiceImpl implements PlaceService {
 
     @Override
     public List<Place> findByRegion(String region) {
+        for (Place place : placeRepository.findByRegion(region)) {
+            
+        }
+
         return List.of();
     }
 }
