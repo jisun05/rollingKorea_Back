f71068b7a2cdcf5563ff29b6721e99b31c65880d
jisun05 <jisunnala@gmail.com>
2025-02-07 21:29:56 +0900

    fix sentence error  v.0.01

--- src/main/java/history/traveler/rollingkorea/comment/domain/Comment.java
+++ src/main/java/history/traveler/rollingkorea/comment/domain/Comment.java
@@ -26,8 +26,8 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Id //이 엔터티의 기본키, 각 레코드를 고유하게 식별하는데 사용됨,유일성 보장
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  //GeneratedValue는 기본키가 어떻게 생성되는지 정의, 기본키 자동증가를 위해서는 @Id와 같이 써야함
-    //@Id가 없으면 JPA는 어떤 필드가 기본 키인지 알 수 없으므로, @GeneratedValue가 제대로 작동하지 않음
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //GeneratedValue 는 기본키가 어떻게 생성되는지 정의, 기본키 자동증가를 위해서는 @Id와 같이 써야함
+    //@Id가 없으면 JPA는 어떤 필드가 기본 키인지 알 수 없으므로, @GeneratedValue 가 제대로 작동하지 않음
     @Column(name = "comment_id")
     private Long commentId;    //auto increment 쓸 경우 String 타입은 지양한다그래서 변경
 
